3d9f22589e2a4e80beb448e614724f69b168d7d2
mosesprime <huntermoses007@gmail.com>
2025-02-05 14:52:21 -0500

    switch default & fill in assignment sheet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Enter a alphabetical character to convert: ");
+        Scanner scnr = new Scanner(System.in);
+        char input = scnr.next().charAt(0);
+        int n = 0;
+        switch (Character.toUpperCase(input)) {
+            case 'A': case 'B': case 'C':
+                n = 2;
+                break;
+            case 'D': case 'E': case 'F' :
+                n = 3;
+                break;
+            case 'G': case 'H': case 'I':
+                n = 4;
+                break;
+            case 'J': case 'K': case 'L':
+                n = 5;
+                break;
+            case 'M': case 'N': case 'O':
+                n = 6;
+                break;
+            case 'P': case 'Q': case 'R': case 'S':
+                n = 7;
+                break;
+            case 'T': case 'U': case 'V':
+                n = 8;
+                break;
+            case 'W': case 'X': case 'Y': case 'Z':
+                n = 9;
+                break;
+            default:
+                System.out.println("Invalid character");
+                System.exit(1);
+        }
+        System.out.println(n);
     }
 }
